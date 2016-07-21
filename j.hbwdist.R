582eeb11132ff0a5a5631b3aa0a79a324a15c11d
bstabler <benstabler@yahoo.com>
2016-07-21 11:08:13 -0700

    fix hbw typo

--- j.hbwdist.R
+++ j.hbwdist.R
@@ -47,7 +47,7 @@ mf.util <- exp (sweep (2.235*mf.hwlsl - 0.4198*(mf.hwlsl^2) + 0.02220*(mf.hwlsl^
 # Distribution Utility Summary
 ma.utsum <- apply (mf.util, 1, sum)
 
-ma.hwldcls <- log(ma.utsum)
+ma.hbwldcls <- log(ma.utsum)
 save (ma.hbwldcls, file="ma.hbwldcls.dat")
 write.table(ma.hbwldcls, sep=",", row.names=F, file="ma.hbwldcls.csv", col.names=c("hbwldcls"))
 
@@ -74,7 +74,7 @@ mf.util <-exp (sweep (2.097*mf.hwlsm - 0.3995*(mf.hwlsm^2) + 0.02524*(mf.hwlsm^3
 # Distribution Utility Summary
 ma.utsum <- apply (mf.util, 1, sum)
 
-ma.hwmdcls <- log(ma.utsum)
+ma.hbwmdcls <- log(ma.utsum)
 save (ma.hbwmdcls, file="ma.hbwmdcls.dat")
 write.table(ma.hbwmdcls, sep=",", row.names=F, file="ma.hbwmdcls.csv", col.names=c("hbwmdcls"))
 
@@ -101,7 +101,7 @@ mf.util <-exp (sweep (1.777*mf.hwlsh  - 0.3908*(mf.hwlsh^2) + 0.02555*(mf.hwlsh^
 # Distribution Utility Summary
 ma.utsum <- apply (mf.util, 1, sum)
 
-ma.hwhdcls <- log(ma.utsum)
+ma.hbwhdcls <- log(ma.utsum)
 save (ma.hbwhdcls, file="ma.hbwhdcls.dat")
 write.table(ma.hbwhdcls, sep=",", row.names=F, file="ma.hbwhdcls.csv", col.names=c("hbwhdcls"))
 
