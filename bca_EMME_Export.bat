306b21b1577979bbd6ef1efcb0e6c55a61439679
bstabler <benstabler@yahoo.com>
2016-08-02 09:37:39 -0700

    rename link output files

--- bca_EMME_Export.bat
+++ bca_EMME_Export.bat
@@ -11,11 +11,11 @@ SET BUILD_ASSIGN_EMP="C:\projects\metromce\testing\2040NB_Build\model\peak\assig
 rem # base skims, trips, link results
 %EMMEPY% EMXtoOMX.py %BASE_SKIMS_EMP% 4 mfs.omx -e mf1 mf2 mf3 mf4 mf5 mf6 mf7 mf8 mf10 mf11 mf12 mf13 mf14 mf15 mf16 mf17 mf18 mf19 mf20 mf21 mf22 mf23 mf24 mf25 mf26 mf27 mf28 mf29 mf30 mf31 mf32 mf33 mf34 mf35 mf36 mf37 mf40 mf41 mf42 mf43 mf44 mf45 mf46 mf47 mf51 mf52 mf60 mf61 mf62 mf63
 %EMMEPY% EMXtoOMX.py %BASE_ASSIGN_EMP% 4 mfs.omx -e mf8 mf7 mf65 mf64 mf63 mf62 mf61 mf60 mf6 mf52 mf51 mf50 mf5 mf47 mf46 mf45 mf44 mf43 mf42 mf41 mf40 mf144 mf143 mf142 mf141 mf134 mf133 mf132 mf131 
-%EMMEPY% ExportLinkResultsToCSV.py %BASE_ASSIGN_EMP% 4782 linksPM2.csv
-%EMMEPY% ExportLinkResultsToCSV.py %BASE_ASSIGN_EMP% 4783 linksMD1.csv
+%EMMEPY% ExportLinkResultsToCSV.py %BASE_ASSIGN_EMP% 4782 base_linksPM2.csv
+%EMMEPY% ExportLinkResultsToCSV.py %BASE_ASSIGN_EMP% 4783 base_linksMD1.csv
 
 rem # build skims, trips, link results
 %EMMEPY% EMXtoOMX.py %BUILD_SKIMS_EMP% 4 mfs.omx -e mf1 mf2 mf3 mf4 mf5 mf6 mf7 mf8 mf10 mf11 mf12 mf13 mf14 mf15 mf16 mf17 mf18 mf19 mf20 mf21 mf22 mf23 mf24 mf25 mf26 mf27 mf28 mf29 mf30 mf31 mf32 mf33 mf34 mf35 mf36 mf37 mf40 mf41 mf42 mf43 mf44 mf45 mf46 mf47 mf51 mf52 mf60 mf61 mf62 mf63
 %EMMEPY% EMXtoOMX.py %BUILD_ASSIGN_EMP% 4 mfs.omx -e mf8 mf7 mf65 mf64 mf63 mf62 mf61 mf60 mf6 mf52 mf51 mf50 mf5 mf47 mf46 mf45 mf44 mf43 mf42 mf41 mf40 mf144 mf143 mf142 mf141 mf134 mf133 mf132 mf131 
-%EMMEPY% ExportLinkResultsToCSV.py %BUILD_ASSIGN_EMP% 4782 linksPM2.csv
-%EMMEPY% ExportLinkResultsToCSV.py %BUILD_ASSIGN_EMP% 4783 linksMD1.csv
+%EMMEPY% ExportLinkResultsToCSV.py %BUILD_ASSIGN_EMP% 4782 build_linksPM2.csv
+%EMMEPY% ExportLinkResultsToCSV.py %BUILD_ASSIGN_EMP% 4783 build_linksMD1.csv
