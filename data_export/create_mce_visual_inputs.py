26be335f0b1678314bb383e0d6864cc5a61ecd2e
bstabler <benstabler@yahoo.com>
2018-12-06 17:37:02 -0800

    updates to the viz export script

--- data_export/create_mce_visual_inputs.py
+++ data_export/create_mce_visual_inputs.py
@@ -1,7 +1,7 @@
 # Create MCE Visualization Dashboard Input Tables
 # Mike Dailey, Ben Stabler, RSG, 02/02/18
-# python create_mce_visual_inputs.py benefits_file zone_benefits_file counties_and_cocs_file od_districts_benefits_file ABMViz_Region.json_File_Local_Location New_Scenario_Name
-# python create_mce_visual_inputs.py final_aggregate_results.csv final_aggregate_zone_summary.csv cocs.csv final_aggregate_od_district_summary.csv C:\SVN\ABMVIZ\data\portland\region.json I205test
+# python create_mce_visual_inputs.py benefits_file zone_benefits_file counties_and_cocs_file od_districts_benefits_file ithim_file ABMViz_Region.json_File_Local_Location New_Scenario_Name
+# python create_mce_visual_inputs.py final_aggregate_results.csv final_aggregate_zone_summary.csv cocs.csv final_aggregate_od_district_summary.csv dalys.csv C:\SVN\ABMVIZ\data\portland\region.json I205test
 # Outputs 3DAnimatedMapData.csv, BarChartAndMapData.csv, BarChartData.csv, ChordData.csv
 
 import os, sys
@@ -15,13 +15,13 @@ import json
 sys.path.append(os.getcwd())
 sys.path.append(os.path.join(os.getcwd(),"inputs"))
 
-def buildBarChartFile(fileName,regionfileloc,datasetname):
+def buildBarChartFile(fileName,ithim_file,regionfileloc,datasetname):
     directory = regionfileloc.replace('region.json', '')
     
     BENEFIT_GROUPS = {'travel_options_benefit': 'Social Goods',
-    	                'physical_activity_benefit': 'Social Goods',
-    	                'safety_cost_benefit': 'Social Goods', 
-    	                'hhs_for_the_coc': 'Social Goods',
+                      'physical_activity_benefit': 'Social Goods',
+                      'safety_cost_benefit': 'Social Goods', 
+                      'hhs_for_the_coc': 'Social Goods',
                       'veh_ownership_cost_benefit': 'Economic Vitality', 
                       'travel_time_reliability_benefit': 'Economic Vitality', 
                       'travel_time_benefit': 'Economic Vitality',
@@ -39,18 +39,20 @@ def buildBarChartFile(fileName,regionfileloc,datasetname):
                       'emissions_cost_benefit': 'Environmental Stewardship', 
                       'noise_pollution_cost_benefit': 'Environmental Stewardship',
                       'surface_water_pollution_cost_benefit': 'Environmental Stewardship'
-    }
+                     }
                       
-    RENAME_BENEFITS = [
-        'sch_travel_time_benefit',    
-        'hbc_travel_time_benefit', 
-        'hbr_travel_time_benefit',
-        'hbs_travel_time_benefit',
-        'hbo_travel_time_benefit', 
-        'nhbnw_travel_time_benefit',
-        'nhbw_travel_time_benefit',
-        'hbw_travel_time_benefit'
-    ]
+    RENAME_BENEFITS = ['sch_travel_time_benefit',
+                       'hbc_travel_time_benefit', 
+                       'hbr_travel_time_benefit',
+                       'hbs_travel_time_benefit',
+                       'hbo_travel_time_benefit', 
+                       'nhbnw_travel_time_benefit',
+                       'nhbw_travel_time_benefit',
+                       'hbw_travel_time_benefit'
+                      ]
+    
+    ithim_benefit = pd.read_csv(ithim_file)['dollars'][0]
+    ithim_rec = pd.DataFrame({"BENEFIT":"active trans", "BENEFIT GROUP":"Everyone", "":ithim_benefit, "CHART":"Benefits"}, index=[0])
     
     with open(fileName,'r') as sourcefile:
         benefits = pd.read_csv(sourcefile)
@@ -66,21 +68,23 @@ def buildBarChartFile(fileName,regionfileloc,datasetname):
         df = df[cols]
         df = df.fillna(0)
         df.columns.values[0] = 'BENEFIT'
+        ithim_rec.columns = df.columns
+        df = df.append(ithim_rec)
         df.to_csv(directory+datasetname+"\BarChartData.csv", encoding='utf-8', index=False)
 
 def animatedMapData(fileName,regionfileloc,datasetname):
     directory = regionfileloc.replace('region.json', '')
     
     BENEFIT_COLUMNS = {'travel_options_benefit': 'ALL PURPOSES',
-    	                 'access_benefit_hbc': 'HBC',
-    	                 'access_benefit_hbo': 'HBO',
-    	                 'access_benefit_hbr': 'HBR',
-    	                 'access_benefit_hbs': 'HBS',
+                       'access_benefit_hbc': 'HBC',
+                       'access_benefit_hbo': 'HBO',
+                       'access_benefit_hbr': 'HBR',
+                       'access_benefit_hbs': 'HBS',
                        'access_benefit_hbw': 'HBW',
                        'access_benefit_nhbnw': 'NHBNW',
                        'access_benefit_nhbw': 'NHBW',
                        'access_benefit_sch': 'SCH'
-    }
+                      }
 
     with open(fileName, 'r') as sourcefile:
         animated = pd.read_csv(sourcefile, usecols=list(BENEFIT_COLUMNS))
@@ -101,14 +105,14 @@ def barchartMap(fileName, countyFile,regionfileloc,datasetname):
     directory = regionfileloc.replace('region.json', '')
     
     BENEFIT_COLUMNS = {'access_benefit_hbc':'HBC',
-    	                 'access_benefit_hbo':'HBO',
-    	                 'access_benefit_hbr':'HBR',
-    	                 'access_benefit_hbs':'HBS',
+                       'access_benefit_hbo':'HBO',
+                       'access_benefit_hbr':'HBR',
+                       'access_benefit_hbs':'HBS',
                        'access_benefit_hbw':'HBW',
                        'access_benefit_nhbnw':'NHBNW',
                        'access_benefit_nhbw':'NHBW',
                        'access_benefit_sch':'SCH'
-    }
+                      }
     
     countys = []
     with open (countyFile,'r') as cntyFile:
@@ -144,8 +148,8 @@ def chordData(od_districts_benefits_file,regionfileloc,datasetname):
     masterFrame.columns = ["FROM","TO","Travel Time Savings"]
     masterFrame.to_csv(directory+datasetname+"\\ChordData.csv", encoding='utf-8', index=False)
     
-def runConvertData(benefitsFile,zoneBenefitsFile,county_file,od_districts_benefits_file,regionfileloc,datasetname):
-    buildBarChartFile(benefits_file,regionfileloc,datasetname)
+def runConvertData(benefitsFile,zoneBenefitsFile,county_file,od_districts_benefits_file,ithim_file,regionfileloc,datasetname):
+    buildBarChartFile(benefits_file,ithim_file,regionfileloc,datasetname)
     animatedMapData(zoneBenefitsFile,regionfileloc,datasetname)
     barchartMap(zoneBenefitsFile,county_file,regionfileloc,datasetname)
     chordData(od_districts_benefits_file,regionfileloc,datasetname)
@@ -171,7 +175,8 @@ if __name__ == "__main__":
     zone_benefits_file = sys.argv[2]
     county_file = sys.argv[3]
     od_districts_benefits_file = sys.argv[4]
-    regionfileloc = sys.argv[5]
-    datasetname = sys.argv[6]
+    ithim_file = sys.argv[5]
+    regionfileloc = sys.argv[6]
+    datasetname = sys.argv[7]
     editRegionFile(regionfileloc,datasetname,county_file)
-    runConvertData(benefits_file,zone_benefits_file,county_file,od_districts_benefits_file,regionfileloc,datasetname)
+    runConvertData(benefits_file,zone_benefits_file,county_file,od_districts_benefits_file,ithim_file,regionfileloc,datasetname)
